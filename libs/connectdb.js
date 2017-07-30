43c2034ac34122d8641e5d2ea55867353756fe9a
Renan Arimathea <renanthea@hotmail.com>
2017-07-29 23:54:54 -0300

    primeiro commit

---
+++ libs/connectdb.js
@@ -0,0 +1,84 @@
+
+/*
+	Arquivo de conexão para o MariaDb
+*/
+
+var myArgs = process.argv.slice(2);
+var port = 0;
+
+// if(myArgs[0] == '-port'){
+// 	port = myArgs[1]
+// }
+// else{
+// 	port = '3306'
+// }
+
+// port = process.env.PORT || 3306
+
+// console.log('Porta para o banco de dados: '+port)
+
+// module.exports = function(){
+
+// 	var mysql      = require('mysql');
+// 	var connection = mysql.createConnection({
+// 		host     : 'localhost',
+// 		user     : 'root',
+// 		password : '',
+// 		database : 'hacka_ima',
+// 		port: port
+// 	});
+
+// 	connection.connect(function(err) {
+// 		if (err) {
+// 			console.error('error connecting: ' + err.stack);
+// 			return;
+// 		}
+
+// 		console.log('Conexão realizada com sucesso!');
+
+// 	});
+
+// 	return connection
+// }
+
+module.exports = function(){
+
+	// var MongoClient = require('mongodb').MongoClient;
+
+	// MongoClient.connect('mongodb://heroku_68zc1kd4:i4shak80loe4avq30n20ik1ak5@ds129013.mlab.com:29013/heroku_68zc1kd4', function(err, db) {
+	// if (err) {
+	// 	throw err;
+	// }
+
+	// console.log('MongoDb conectado com sucesso')
+	// console.log('=========================================================')
+
+	
+	
+	// return MongoClient
+
+	// })
+
+	var mongoose = require('mongoose')
+	mongoose.Promise = global.Promise
+
+	var uri = 'mongodb://heroku_68zc1kd4:i4shak80loe4avq30n20ik1ak5@ds129013.mlab.com:29013/heroku_68zc1kd4'
+
+	mongoose.connect(uri).then(function(){
+
+		console.log('MongoDb conectado com sucesso!!!')
+
+		// ### Registra Schemas ###
+
+		// Perguntas
+		var perguntasSchema = new mongoose.Schema()
+		mongoose.model('Perguntas', perguntasSchema)
+
+		
+
+
+	})
+
+	return mongoose.connection
+
+}
---
+++ routes/index.js
@@ -0,0 +1,225 @@
+
+module.exports = function(app){
+
+  // Importa modulos nativos
+  var rp = require('request-promise');
+  var request = require('request');
+
+  // Custom Modules
+  var db = require('./../libs/connectdb.js')()
+
+  global.registros_novos = []
+
+  app.get('/recupera_perguntas', function(req, res){
+
+      var Perguntas = db.model('Perguntas')
+      Perguntas.find({}, function(err, docs){
+      	res.send(docs)
+      })
+
+  })
+
+  app.get('/contratos', function(req, res){
+
+    res.render('contratos')
+
+  })
+
+  app.get('/dashboards', function(req, res){
+
+    res.render('dashboards')
+
+  })
+
+  app.get('/atendimentos_dashboard', function(req, res){
+
+    var requestOptions = {
+      uri: 'http://apigateway.ima.sp.gov.br:8080/apiman-gateway/Hackathon/api/2.0/saude/atendimentos',
+      qs:{
+        apikey: TOKEN_IMA,
+        pagina:1,
+        limite:50
+      },
+      json: true
+    }
+
+    rp(requestOptions).then(function(response){
+
+      var registros = []
+
+      response.content.forEach(function(value, index){
+
+        // var registro = {
+        //   hora: value.hora,
+        //   dataAtendimento: value.dataAtendimento,
+        //   dataNascimento: value.dataNascimento,
+        //   sexo: value.sexo,
+        //   periodo: value.periodo,
+        //   localVinculo: value.localVinculo,
+        //   distritoAtendimento: value.distritoAtendimento,
+        //   procedimento: value.procedimento
+
+        // }
+
+        registros.push(registro)
+
+      })
+
+      res.send(registros)
+    })
+
+  })
+
+  app.get('/atendimentos_lista', function(req, res){
+
+    var requestOptions = {
+      uri: 'http://apigateway.ima.sp.gov.br:8080/apiman-gateway/Hackathon/api/2.0/saude/atendimentos',
+      qs:{
+        apikey: TOKEN_IMA,
+        pagina:1,
+        limite:50
+      },
+      json: true
+    }
+
+    rp(requestOptions).then(function(response){
+
+      // Recupera os dados da base de novos registros
+      var registros = []
+
+      global.registros_novos.forEach(function(value, index){
+
+          var registro = {
+          hora: value.hora,
+          dataAtendimento: value.dataAtendimento,
+          dataNascimento: value.dataNascimento,
+          sexo: value.sexo,
+          periodo: value.periodo,
+          localVinculo: value.localVinculo,
+          distritoAtendimento: value.distritoAtendimento,
+          procedimento: value.procedimento
+
+        }
+
+        registros.push(registro)
+
+      })
+
+      // Recupera os dados da base de atendimentos da IMA
+      response.content.forEach(function(value, index){
+
+        var registro = {
+          hora: value.hora,
+          dataAtendimento: value.dataAtendimento,
+          dataNascimento: value.dataNascimento,
+          sexo: value.sexo,
+          periodo: value.periodo,
+          localVinculo: value.localVinculo,
+          distritoAtendimento: value.distritoAtendimento,
+          procedimento: value.procedimento
+
+        }
+
+        registros.push(registro)
+
+      })
+
+      res.send(registros)
+    })
+
+  })
+
+
+  // Cria um registro de atendimento pela API
+
+  app.get('/adiciona_atendimento', function(req, res){
+
+    var requestBody = ''
+
+    if(Object.keys(req.query).length == 0){
+
+      requestBody = {
+        "atividadeProfissional": "string",
+        "cbo": "string",
+        "codigoAtividadeProfissional": "string",
+        "codigoGrupoAtendimento": "15",
+        "codigoProcedimento": "string",
+        "codigoSexo": "1",
+        "codigoTipoAtendimento": "10",
+        "dataAtendimento": "09/07/2017",
+        "dataNascimento": "05/02/1997",
+        "distritoAtendimento": "CENTRO",
+        "distritoVinculo": "string",
+        "grupoAtendimento": "string",
+        "hora": "13H",
+        "idade": "string",
+        "localAtendimento": "string",
+        "localVinculo": "HOSPITAL MARIO GATTI",
+        "municipio": "string",
+        "periodo": "MANHA",
+        "procedimento": "CONSULTA MEDICA",
+        "qtdRealizada": 1,
+        "sexo": "MASCULINO",
+        "tipoAtendimento": "string",
+        "tipoVinculo": "string",
+        "uf": "string"
+      }
+    }
+    else{
+
+        requestBody = {
+        "atividadeProfissional": "string",
+        "cbo": "string",
+        "codigoAtividadeProfissional": "string",
+        "codigoGrupoAtendimento": "15",
+        "codigoProcedimento": "string",
+        "codigoSexo": "1",
+        "codigoTipoAtendimento": "10",
+        "dataAtendimento": "09/07/2017",
+        "dataNascimento": "29/11/1989",
+        "distritoAtendimento": "string",
+        "distritoVinculo": "string",
+        "grupoAtendimento": "string",
+        "hora": "13H",
+        "idade": "string",
+        "localAtendimento": "string",
+        "localVinculo": "string",
+        "municipio": "string",
+        "periodo": "string",
+        "procedimento": "string",
+        "qtdRealizada": 1,
+        "sexo": "M",
+        "tipoAtendimento": "string",
+        "tipoVinculo": "string",
+        "uf": "string"
+      }
+
+    }
+
+    var requestOptions = {
+      method: 'POST',
+      uri: 'http://apigateway.ima.sp.gov.br:8080/apiman-gateway/Hackathon/api/2.0/saude/atendimentos',
+      qs:{
+        apikey: TOKEN_IMA,
+      },
+      body: requestBody,
+      json: true
+    }
+
+    rp(requestOptions).then(function(response){
+      
+      console.log('RESULTADO DO ENVIO DA TENTATIVA DE CADASTRO DE ATENDIMENTO')
+      console.log('===============================================================================')
+      console.log(response)
+
+      global.registros_novos.push(requestBody)
+
+      console.log(global.registros_novos)
+
+      res.send(response)
+
+    })
+
+  })
+
+}
